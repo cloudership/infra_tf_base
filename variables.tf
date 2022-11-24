364d28d333f02d471593642a8cb05343a7812978
Asfand Qazi <asfand@cloudership.com>
2022-11-24 11:43:37 +0000

    Define a basic VPC

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  type = string
+}
+
+variable "env_name" {
+  type = string
+}
+
+variable "aws_availability_zone_letters" {
+  type = list(string)
+  description = <<-EOT
+    The letters of the availability zones to use, e.g. ["a", "b", "c"]
+  EOT
+  default = ["a", "b", "c"]
+}
+
+variable "vpc_subnet_address" {
+  type = string
+  description = "Subnet address for the VPC, e.g. 10.127.0.0"
+}
+
+variable "vpc_subnet_mask_bits" {
+  type = number
+  description = "Subnet mask bits e.g. 16. Recommended this be set to 16"
+}
