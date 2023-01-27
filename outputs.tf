6176c6fbbe20b5bcfd9c6993dc8440b0b4d6121c
Asfand Qazi <asfand@cloudership.com>
2023-01-27 18:21:08 +0000

    Subnet IDs added to outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "public_subnet_arns" {
   value = module.vpc.public_subnet_arns
 }
 
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
 output "private_subnet_arns" {
   value = module.vpc.private_subnet_arns
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
 }
