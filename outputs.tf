fedba750bc6417a684ebe951392deb0ab0be4602
Asfand Qazi <asfand@cloudership.com>
2023-02-03 22:07:52 +0000

    Outputs declared to allow building simple services
    This is based on the sample_web_service experiment

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "public_subnet_arns" {
   value = module.vpc.public_subnet_arns
 }
@@ -16,4 +20,21 @@ output "private_subnet_arns" {
 
 output "private_subnet_ids" {
   value = module.vpc.private_subnets
+}
+
+output "public_alb" {
+  value = {
+    sg_internal_id     = local.public_alb_sg_internal_id
+    https_listener_arn = local.public_alb_https_listener_arn
+    lb_dns_name        = module.public_alb.lb_dns_name
+    lb_zone_id         = module.public_alb.lb_zone_id
+  }
+}
+
+output "ecs_cluster_default_arn" {
+  value = aws_ecs_cluster.default.arn
+}
+
+output "route53_zone_public_id" {
+  value = aws_route53_zone.public.id
 }
