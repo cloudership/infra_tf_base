bf43becee2ada246f392d81c713f6911aee2cf56
Asfand Qazi <ayqazi@gmail.com>
2024-07-29 21:02:31 +0100

    Explain subnet division

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 locals {
-  vpc_cidr         = "${var.subnet_address}/${var.subnet_mask_bits}"
+  vpc_cidr = "${var.subnet_address}/${var.subnet_mask_bits}"
+
+  # First divide vpc_cidr into 2 subnets of equal size (1 bit smaller than vpc_cidr) for public and private subnets.
+  # Then divide each of the public and private subnets into 3 smaller subnets, each 2 bits smaller than those, so both
+  # public and private subnets have 3 equal-sized smaller subnets, one for each AZ.
   subnet_addresses = [for cidr_block in cidrsubnets(local.vpc_cidr, 1, 1) : cidrsubnets(cidr_block, 2, 2, 2)]
 
   tags = {
