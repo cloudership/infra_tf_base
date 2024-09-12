01621f32e020d342fb32308496f4dc5a8010eb9c
Asfand Qazi <ayqazi@gmail.com>
2024-09-12 12:36:28 +0100

    eks: rename outputs and output oidc provider and cluster info

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,3 +1,15 @@
-output "main_cluster_name" {
+output "eks_cluster_main_name" {
   value = module.main_eks_cluster.cluster_name
+}
+
+output "eks_cluster_main_arn" {
+  value = module.main_eks_cluster.cluster_arn
+}
+
+output "eks_cluster_main_oidc_provider_name" {
+  value = module.main_eks_cluster.oidc_provider
+}
+
+output "eks_cluster_main_oidc_provider_arn" {
+  value = module.main_eks_cluster.oidc_provider_arn
 }
