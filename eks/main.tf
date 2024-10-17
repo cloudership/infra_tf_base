d28cbf0c61d8a64c75baa2b9f324e38ee343378e
Asfand Qazi <ayqazi@gmail.com>
2024-10-17 22:12:57 +0100

    Bump eks kubernetes version to 1.31

--- eks/main.tf
+++ eks/main.tf
@@ -16,7 +16,7 @@ module "main_eks_cluster" {
   version = "~> 20.24"
 
   cluster_name    = "${var.project_name}-main"
-  cluster_version = "1.30"
+  cluster_version = "1.31"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
